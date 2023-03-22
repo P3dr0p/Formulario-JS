bd18f97bf9c89ccbe5a312b5a646708e0fa5a993
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-03-22 08:22:31 -0300

    Add files via upload

--- HighSchool/JS/script.js
+++ HighSchool/JS/script.js
@@ -1,5 +1,5 @@
 const Email = document.getElementById('Email');
 const Password = document.getElementById('Password');
 
-Email Regex: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-Password Regex: ^[A-Za-z0-9]\w{8,}$/
+Regex: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+Regex: /^[A-Za-z0-9]\w{8,}$/
